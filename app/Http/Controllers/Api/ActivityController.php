a835abbeb373ad12f9423304b98ee37784690b4b
aleksandranickij <yanitsky.a@easydev.group>
2025-02-11 15:41:58 +0300

    bugfix

--- app/Http/Controllers/Api/ActivityController.php
+++ app/Http/Controllers/Api/ActivityController.php
@@ -32,28 +32,12 @@ class ActivityController extends Controller
      *             @OA\Property(property="id", description="Уникальный идентификатор вида деятельности", type="integer", example=1),
      *             @OA\Property(property="parent_id", description="ID родительской деятельности, если есть", type="integer", example=1),
      *             @OA\Property(property="name", description="Название вида деятельности", type="string", example="ООО Рога и Копыта"),
-     *             @OA\Property(property="children", type="array", collectionFormat="multi",
-     *                @OA\Items(type="object",
-     *                   @OA\Property(property="id", type="integer", description="Уникальный идентификатор вида деятельности", example=1),
-     *                   type="object",
-     *                      @OA\Property(property="name", type="string", description="Название вида деятельности", example="Мясная продукция"),
-     *                   type="object",
-     *                       @OA\Property(property="parent_id", type="integer", description="ID родительской деятельности, если есть", example=1),
-     *                   type="object",
-     *                       @OA\Property(property="created_at", type="string", example="2025-05-20T14:00:00.000000Z"),
-     *                   type="object",
-     *                       @OA\Property(property="updated_at", type="string", example="2025-05-20T14:00:00.000000Z"),
-     *               ),
-     *           ),
-     *        ),
-     *        @OA\Property(property="created_at", type="string", example="2025-05-20T14:00:00.000000Z"),
-     *        @OA\Property(property="updated_at", type="string", example="2025-05-20T14:00:00.000000Z")
-     *     )
+     *             @OA\Property(property="created_at", type="string", example="2025-05-20T14:00:00.000000Z"),
+     *             @OA\Property(property="updated_at", type="string", example="2025-05-20T14:00:00.000000Z")
+     *         )
      *     )
      * )
      */
-
-
     public function index(): JsonResponse
     {
         return response()->json($this->activityService->getAllActivities());
@@ -65,14 +49,28 @@ class ActivityController extends Controller
      *     summary="Дерево видов деятельности",
      *     @OA\Response(
      *         response=200,
-     *         description="Уникальный идентификатор вида деятельности",
+     *         description="Получить дерево видов деятельности",
      *         @OA\JsonContent(
      *             @OA\Property(property="id", description="Уникальный идентификатор вида деятельности", type="integer", example=1),
      *             @OA\Property(property="parent_id", description="ID родительской деятельности, если есть", type="integer", example=1),
      *             @OA\Property(property="name", description="Название вида деятельности", type="string", example="ООО Рога и Копыта"),
-     *             @OA\Property(property="created_at", type="string", example="2024-05-20T14:00:00.000000Z"),
-     *             @OA\Property(property="updated_at", type="string", example="2024-05-20T14:00:00.000000Z")
-     *         )
+     *             @OA\Property(property="children", type="array", collectionFormat="multi",
+     *                @OA\Items(type="object",
+     *                   @OA\Property(property="id", type="integer", description="Уникальный идентификатор вида деятельности", example=1),
+     *                   type="object",
+     *                      @OA\Property(property="name", type="string", description="Название вида деятельности", example="Мясная продукция"),
+     *                   type="object",
+     *                       @OA\Property(property="parent_id", type="integer", description="ID родительской деятельности, если есть", example=1),
+     *                   type="object",
+     *                       @OA\Property(property="created_at", type="string", example="2025-05-20T14:00:00.000000Z"),
+     *                   type="object",
+     *                       @OA\Property(property="updated_at", type="string", example="2025-05-20T14:00:00.000000Z"),
+     *                  ),
+     *               ),
+     *           ),
+     *           @OA\Property(property="created_at", type="string", example="2025-05-20T14:00:00.000000Z"),
+     *           @OA\Property(property="updated_at", type="string", example="2025-05-20T14:00:00.000000Z")
+     *        )
      *     )
      * )
      */
--- app/Http/Controllers/Api/BuildingController.php
+++ app/Http/Controllers/Api/BuildingController.php
@@ -22,7 +22,22 @@ class BuildingController extends Controller
     }
 
     /**
-     * @return JsonResponse
+     * @OA\Get(
+     *     path="/api/v1/buildings",
+     *     summary="Список всех зданий",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Получить список всех зданий",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="id", description="Уникальный идентификатор здания", type="integer", example=1),
+     *             @OA\Property(property="address", description="Адрес здания", type="string", example="г. Москва, ул. Ленина 1, офис 3"),
+     *             @OA\Property(property="latitude", description="", type="float", example="55.7558000"),
+     *             @OA\Property(property="longitude", description="", type="float", example="37.6173000"),
+     *             @OA\Property(property="created_at", type="string", example="2025-05-20T14:00:00.000000Z"),
+     *             @OA\Property(property="updated_at", type="string", example="2025-05-20T14:00:00.000000Z")
+     *         )
+     *     ),
+     * )
      */
     public function index(): JsonResponse
     {
